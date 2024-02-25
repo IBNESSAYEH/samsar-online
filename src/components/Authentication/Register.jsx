b70de1f16e95f54217a3680ffea5f70cea5da5f2
IBNESSAYEH <abdellatifibnessayeh@gmail.com>
2024-02-25 12:21:37 +0100

    add AuthContext provide for login/register components

--- src/components/Authentication/Register.jsx
+++ src/components/Authentication/Register.jsx
@@ -1,178 +1,17 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useContext} from 'react';
 import axios from "axios"
+import AuthContext from '../../contexts/AuthenticationContext/AuthContexts';
+
 
 const Register = () => {
-  //--------------------------state init------------------------------------
-  // inputs references initializing
-  const firstNameInput = useRef("");
-  const lastNameInput = useRef("");
-  const phoneInput = useRef("");
-  const emailInput = useRef("");
-  const passwordInput = useRef("");
-  const confirmPasswordInput = useRef("");
+   
+  // destruct AuthContext data
+ const { firstNameInput,lastNameInput,phoneInput,emailInput,passwordInput,confirmPasswordInput,
+  firstNameInputMessage,lastNameInputMessage,phoneInputMessage,emailInputMessage,
+  passwordInputMessage,confirmPasswordInputMessage,handleFirstNameInput,handleLastNameInput,
+  handlePhoneInput,handleRegisterEmailInput,handlePasswordInput,handleConfirmPasswordInput,
+  handleSubmit} = useContext(AuthContext)
 
-  // inputs messages states initializing
-  const [firstNameInputMessage, setFirstNameInputMessage] = useState("");
-  const [lastNameInputMessage, setLastNameInputMessage] = useState("");
-  const [phoneInputMessage, setPhoneInputMessage] = useState("");
-  const [emailInputMessage, setEmailInputMessage] = useState("");
-  const [passwordInputMessage, setPasswordInputMessage] = useState([]);
-  const [confirmPasswordInputMessage, setConfirmPasswordInputMessage] = useState("");
-  //----------------------------handlig inputs----------------------------------------
-  const handleFirstNameInput = () => {
-    const regex = /^[a-zA-Z]+$/;
-    if (!regex.test(firstNameInput.current.value.trim())) {
-      setFirstNameInputMessage("First name must contain only letters");
-      return false;
-
-    } else if (firstNameInput.current.value.trim().length < 3) {
-      setFirstNameInputMessage("First name must be at least 3 characters long");
-      return false;
-    } else {
-      setFirstNameInputMessage("");
-      return true;
-    }
-  }
-  const handleLastNameInput = () => {
-    const regex = /^[a-zA-Z]+$/;
-    if (!regex.test(lastNameInput.current.value.trim())) {
-      setLastNameInputMessage("Last name must contain only letters");
-      return false;
-    } else if (lastNameInput.current.value.trim().length < 3) {
-      setLastNameInputMessage("Last name must be at least 3 characters long");
-      return false;
-    } else {
-      setLastNameInputMessage("");
-      return true;
-    }
-  }
-  
-  const validatePhoneNumber = (phone) => {
-    const phoneRegex = /^\+?[0-9\s-()]{7,}$/;
-    return phoneRegex.test(phone);
-  };
-  const handlePhoneInput = () => {
-    if(!validatePhoneNumber(phoneInput.current.value.trim())){
-      setPhoneInputMessage('the phone number must be like this format +212 610703593') 
-      return false;
-    }else{
-      setPhoneInputMessage('');
-      return true;
-    } 
- 
-  }
-
-  const validateEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
-  const handleEmailInput = () => {
-    if (validateEmail(emailInput.current.value.trim())) {
-      setEmailInputMessage("");
-      return true;
-    } else if(firstNameInput.current.value.trim().length > 3 && lastNameInput.current.value.trim().length > 3) {
-      setEmailInputMessage(`the email must be like  ${firstNameInput.current.value.trim()} ${lastNameInput.current.value.trim()}@gmail.com`) 
-      return false;
-    }else{
-      setEmailInputMessage(`the email must be like ahmedibnali123@gmail.com`) 
-      return false;
-    }
-    
-  }
-
-  const validatePassword = (password) => {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return passwordRegex.test(password);
-  };
-
-
-  const handlePasswordInput = () => {
-    if (!validatePassword(passwordInput.current.value.trim())) {
-      setPasswordInputMessage([
-        "Contains at least one lowercase letter.",
-        "Contains at least one uppercase letter.",
-        "Contains at least one digit.",
-        "Contains at least one special character (#&@$)",
-        "Has a minimum length of 8 characters."
-      ]);
-      return false;
-    } else {
-      setPasswordInputMessage([]);
-      return true;
-    }
-  }
-  
-  const handleConfirmPasswordInput = () => {
-    const password = passwordInput.current.value.trim();
-    const confirmPassword = confirmPasswordInput.current.value.trim();
-  
-    if (confirmPassword !== password) {
-      setConfirmPasswordInputMessage("Passwords do not match");
-      return false;
-    } else {
-      setConfirmPasswordInputMessage("");
-      return true;
-    }
-  }
-
-
-
-
-
-  const [formData, setFormData] = useState({});
- 
-  const [formErrors, setFormErrors] = useState({});
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-  
-    // Check if all fields are valid before submission
-    if (
-      handleFirstNameInput() &&
-      handleLastNameInput() &&
-      handlePhoneInput() &&
-      handleEmailInput() &&
-      handlePasswordInput() &&
-      handleConfirmPasswordInput()
-    ) {
-      const formData = {
-        first_name: firstNameInput.current.value,
-        last_name: lastNameInput.current.value,
-        phone: phoneInput.current.value,
-        email: emailInput.current.value,
-        password: passwordInput.current.value,
-      };
-  
-      try {
-        const response = await axios.post("http://localhost:8000/api/register", formData);
-  
-        if (response.status === 200) {
-         
-          alert(response.data.message);
-        } else if (response.status === 422) {
-          // Display validation errors
-          const errorMessages = Object.values(response.data.errors).join('\n');
-          alert(errorMessages);
-        }
-      } catch (error) {
-        if (error.response) {
-          if (error.response.status === 500) {
-            
-            alert("Email already exists. Please use a different email.");
-          } else {
-            const errorMessages = Object.values(error.response.data.errors).join('\n');
-            
-            alert(errorMessages + '\n' + "try again and respect the messages bellow rhe inputs !");
-          }
-        } else {
-          console.error('An error occurred:', error.message);
-         
-        }
-      }
-    } else {
-      alert("Please correct the errors and try again!!");
-    }
-  };
 
 
   return (
@@ -203,7 +42,7 @@ const Register = () => {
             <div className="register__form__group col-12 col-md-6 gap-2">
               <div className="register__form__group__item mb-3">
                 <label htmlFor="exampleInputEmail1" className="register__form__label">Email address :</label>
-                <input type="email" ref={emailInput} onChange={handleEmailInput}  className="register__form__control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email" />
+                <input type="email" ref={emailInput} onChange={handleRegisterEmailInput}  className="register__form__control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email" />
                 {emailInputMessage && ( <small className='text-danger'>{emailInputMessage}</small> )}
 
               </div>
@@ -221,7 +60,7 @@ const Register = () => {
               <div className="register__form__group__item mb-3">
                 <label htmlFor="confirm" className="register__form__label">Confirm Password :</label>
                 <input type="password" ref={confirmPasswordInput} onChange={handleConfirmPasswordInput}  className="register__form__control" id="confirm" placeholder="Repeat your password" />
-                {setConfirmPasswordInputMessage && ( <small className='text-danger'>{confirmPasswordInputMessage}</small> )}
+                {confirmPasswordInputMessage && ( <small className='text-danger'>{confirmPasswordInputMessage}</small> )}
               </div>
             </div>
             <div className="register__form__group col-12 mb-3">
