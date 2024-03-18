ddd60d218c89e7ee8385d8b4660b6c1dcf061981
IBNESSAYEH <abdellatifibnessayeh@gmail.com>
2024-03-18 21:08:01 +0000

    add router folder

---
+++ src/router/index.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home";
+
+export const router = createBrowserRouter([
+    {
+        path : '/',
+        element: <Home/>
+    },
+    {
+        path : '/',
+        element: <Home/>
+    },
+    {
+        path : '/',
+        element: <Home/>
+    },
+])
